dc1ac00f94230b704e76983cd2507ec59dc97c42
TLodosDurak <tlodosdurak@gmail.com>
2023-02-04 16:10:59 -0500

    Add line_number() function and file handling
    	-Create line_number(file1: str, file2: str) that takes as
    		parameters two strings representing file names.
    		The function reads the file indicated by the first parameter
    		and writes its lines prefixed by the line number to the file
    		represented by the second parameter.
    	-Add file handling

--- Problems/p1_Durak_Temel.py
+++ Problems/p1_Durak_Temel.py
@@ -5,3 +5,22 @@ Author: Temel Lodos Durak
 Date: 02/05/23
 
 """
+def line_number(file1: str, file2: str):
+    '''
+    line_number takes as parameters two strings representing file names.
+    The function reads the file indicated by the first parameter and writes its
+    lines prefixed by the line number to the file represented by the second parameter.
+    '''
+    
+    try:
+        input_file = open(file1, "r") 
+    except FileNotFoundError as e:
+        raise e
+        
+    output_file = open(file2, "w")
+    
+    
+#Input file will be this file itself
+file1_str = "p1_Durak_Temel.py"
+file2_str = "p1_Durak_Temel.py.txt" #Output file will be created with this name
+line_number(file1_str, file2_str)
